9c78174992cd0e1e018cb531de6baab4187ee1f1
ChiaraPapagni <chiaraiors@gmail.com>
2021-10-26 10:42:19 +0200

    aggiunti bonus 2 e 3

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,22 +14,22 @@ Un secondo array conterrà solo gli id dei posts a cui abbiamo dato like. */
 const posts =
     [
         {
-            id: 1012,
+            id: 1,
             text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Natus aliquid distinctio, a earum recusandae animi voluptatem saepe impedit quas atque aut, ullam at officia. Totam natus quod illum incidunt sint.',
-            image: 'https://picsum.photos/600/400',
+            image: 'https://picsum.photos/id/1013/600/400',
             author:
             {
                 authorName: 'Phil Mangione',
-                authorImage: 'https://unsplash.it/60/60?image=',
+                authorImage: 'https://picsum.photos/id/1012/60/60',
             }
             ,
             like: 80,
             date: new Date().toLocaleDateString(),
         },
         {
-            id: 1005,
+            id: 2,
             text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit. Cum magnam facilis, ut expedita repudiandae id sed sequi aperiam unde excepturi ipsa laudantium saepe. Assumenda sequi odit natus dolorum, nam dignissimos.',
-            image: 'https://picsum.photos/600/400',
+            image: 'https://picsum.photos/id/225/600/400',
             author:
             {
                 authorName: 'Sofia Perlari',
@@ -37,24 +37,37 @@ const posts =
             }
             ,
             like: 17,
-            date: new Date('2021-10-22').toLocaleDateString(),
+            date: new Date('2021-10-22').toLocaleDateString('it'),
         },
         {
-            id: 1025,
+            id: 3,
             text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque deserunt eius odio ipsum sit ut facere totam quidem consequatur dicta non sint quae dolor, sed iusto sunt laudantium veritatis minus.',
-            image: 'https://picsum.photos/600/400',
+            image: 'https://picsum.photos/id/1008/600/400',
             author:
             {
                 authorName: 'Chiara Ferragni',
-                authorImage: 'https://unsplash.it/60/60?image=',
+                authorImage: 'https://picsum.photos/id/1025/60/60',
             }
             ,
             like: 1589,
             date: new Date('2021-10-21').toLocaleDateString(),
         },
+        {
+            id: 4,
+            text: 'Lorem ipsum dolor sit amet, consectetur adipisicing elit.',
+            image: 'https://picsum.photos/id/26/600/400',
+            author:
+            {
+                authorName: 'Regan Bodween',
+                authorImage: 'https://picsum.photos/id/259/60/60',
+            }
+            ,
+            like: 58,
+            date: new Date('2021-10-21').toLocaleDateString(),
+        },
     ];
 
-const likedPosts = [1012, 1005];
+const likedPosts = [1, 2];
 
 renderPosts(posts);
 
@@ -62,7 +75,7 @@ function renderPost(posts) {
     return `<div class="post">
     <div class="author">
         <div class="author_image">
-            <img src="${posts.author.authorImage}${posts.id}" alt="${posts.author.authorName}">
+            <img src="${posts.author.authorImage}" alt="${posts.author.authorName}">
         </div>
         <div class="name">
             ${posts.author.authorName}
@@ -94,6 +107,21 @@ function renderPosts(posts) {
         if (posts[i].author.authorImage == '') {
             let nameInitials = posts[i].author.authorName.match(/\b(\w)/g).join('');
             document.getElementsByClassName('author_image')[i].innerHTML = nameInitials;
+        }
+    }
+}
+
+
+const likeButtons = document.getElementsByClassName('like_btn');
+for (let i = 0; i < likeButtons.length; i++) {
+    const likeButton = likeButtons[i];
+    likeButton.addEventListener('click', pressLike);
+}
+
+function pressLike() {
+    for (let i = 0; i < posts.length; i++) {
+        if (likeButtons[i].style.color != '#61D2A4') {
+            this.style.color = '#61D2A4';
         }
     }
 }
